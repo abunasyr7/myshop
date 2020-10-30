0dcdf73a1817bac33a015793b36ebaf6ade2d8a5
abunasyr7 <abunasyr1@gmail.com>
2020-10-30 14:58:17 +0600

    first day of work

--- app.js
+++ app.js
@@ -1,61 +1,74 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 //express app
-const app = express ();
+const app = express();
 const mongoose = require('mongoose')
 const expressEjsLayout = require('express-ejs-layouts')
-const flash = require ('connect-flash');
-const session = require ('express-session');
+const flash = require('connect-flash');
+const session = require('express-session');
 const passport = require('passport');
 require("./config/passport")(passport)
 //mongoose
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser:true, useUnifiedTopology: true})
-.then(() => console.log('connected,,'))
-.catch((err) => console.log(err));
+mongoose.connect('mongodb://localhost/test', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('connected,,'))
+    .catch((err) => console.log(err));
 //register view engine
 app.set('view engine', 'ejs');
 app.use(expressEjsLayout);
 //BodyParser
 app.use(expressEjsLayout);
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 //express session
 app.use(session({
-    secret : 'secret',
-    resave : true,
-    saveUninitialized : true
-   }));
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 //use flash
 app.use(flash());
-app.use((req,res,next)=> {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error  = req.flash('error');
-next();
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
 })
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 //listen for request
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is listening to the port 3000');
+    console.log('Server is listening to the port 3000');
 });
 
-app.get('/', (req,res) =>{
-    res.render('homepage', {title: 'MyShop'})
+app.get('/', (req, res) => {
+    res.render('homepage', {
+        title: 'MyShop'
+    })
 });
 
-app.get('/about',(req, res) =>{
-    res.render('about',{title: 'About'});
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About'
+    });
 });
 
-app.get('/welcome',(req, res) =>{
-    res.render('welcome',{title: 'Welcome'});
+app.get('/welcome', (req, res) => {
+    res.render('welcome', {
+        title: 'Welcome'
+    });
 });
 
-app.get('/contact',(req, res) =>{
-    res.render('contact',{title: 'Contacts'});
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contacts'
+    });
 });
 
 //404 page
--- routes/users.js
+++ routes/users.js
@@ -1,85 +1,106 @@
 const User = require("../models/user.js")
 const express = require('express');
-const { render } = require("ejs");
+const {
+    render
+} = require("ejs");
 const router = express.Router();
-const bcrypt = require ('bcrypt');
+const bcrypt = require('bcrypt');
 const passport = require('passport');
 const title = 'test'
 
 //login handle
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
     res.render('login');
 })
-router.get('/register',(req,res)=>{
-    res.render('register', {title: title});})
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: title
+    });
+})
 //Register handle
-router.post('/register',(req,res)=>{
-    const {name, email, password, password2} = req.body;
+router.post('/register', (req, res) => {
+    const {
+        name,
+        email,
+        password,
+        password2
+    } = req.body;
     let errors = [];
-    console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
-    if(!name || !email || !password || !password2) {
-        errors.push ({msg : "Please fill in all fields"})
+    console.log(' Name ' + name + ' email :' + email + ' pass:' + password);
+    if (!name || !email || !password || !password2) {
+        errors.push({
+            msg: "Please fill in all fields"
+        })
     }
-//check if match
-    if(password !== password2) {
-        errors.push({msg : "passwords dont match"});
+    //check if match
+    if (password !== password2) {
+        errors.push({
+            msg: "passwords dont match"
+        });
     }
-//check if password is more than 6 characters
-    if(password.length < 6 ) {
-        errors.push({msg : 'password atleast 6 characters'})
+    //check if password is more than 6 characters
+    if (password.length < 6) {
+        errors.push({
+            msg: 'password atleast 6 characters'
+        })
     }
-    if (errors. length > 0){
+    if (errors.length > 0) {
         res.render('register', {
             title: title,
             errors: errors,
             name: name,
             email: email,
-            password : password,
-            password2 : password2})
+            password: password,
+            password2: password2
+        })
     } else {
         //validation passed
-        User.findOne ({email: email}).exec((err,user)=> {
+        User.findOne({
+            email: email
+        }).exec((err, user) => {
             console.log(user);
             if (user) {
-                errors.push({msg: 'email already registred'});
-                render(res, errors, name, email, password, password2);
-            } else{
-                const newUser = new User({
-                    name : name,
-                    email : email,
-                    password : password
+                errors.push({
+                    msg: 'email already registred'
                 });
-                bcrypt.genSalt(10,(err,salt)=>
-                bcrypt.hash(newUser.password,salt,
-                    (err,hash)=> {
-                        if(err) throw err;
+                render(res, errors, name, email, password, password2);
+            } else {
+                const newUser = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+                bcrypt.genSalt(10, (err, salt) =>
+                    bcrypt.hash(newUser.password, salt,
+                        (err, hash) => {
+                            if (err) throw err;
                             //save pass to hash
                             newUser.password = hash;
-                        //save user
-                        newUser
-                        .save()
-                        .then((value)=>{
-                            console.log(value)
-                            req.flash('success_msg','You have now registered!')
-                            res.redirect('/users/login');
-                        })
-                        .catch(value=> console.log(value));
+                            //save user
+                            newUser
+                                .save()
+                                .then((value) => {
+                                    console.log(value)
+                                    req.flash('success_msg', 'You have now registered!')
+                                    res.redirect('/users/login');
+                                })
+                                .catch(value => console.log(value));
 
-                    }));
+                        }));
             }
         })
     }
 })
-router.post('/login',(req,res,next)=>{
-    passport.authenticate('local',{
-        successRedirect : '/dashboard',
-        failureRedirect : '/users/login',
-        failureFlash : true,
-        })(req,res,next);
-  })
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true,
+    })(req, res, next);
+})
 
 //logout
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
 
- })
-module.exports  = router;
+})
+module.exports = router;
